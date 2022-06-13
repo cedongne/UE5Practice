48f31fcd428b96b33d29d74751149edf33881b4d
cedongne <cedongne@gmail.com>
2022-06-13 10:35:42 +0900

    NextContent

--- Source/UE5Practice/UE5Practice.cpp
+++ Source/UE5Practice/UE5Practice.cpp
@@ -3,5 +3,8 @@
 #include "UE5Practice.h"
 #include "Modules/ModuleManager.h"
 
+DEFINE_LOG_CATEGORY(UE5Practice);
+
+
 IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UE5Practice, "UE5Practice" );
  
--- Source/UE5Practice/UE5Practice.h
+++ Source/UE5Practice/UE5Practice.h
@@ -2,4 +2,10 @@
 
 #pragma once
 
-#include "CoreMinimal.h"
+#include "EngineMinimal.h"
+
+DECLARE_LOG_CATEGORY_EXTERN(UE5Practice, Log, All);
+
+#define ABLOG_CALLINFO (FString(__FUNCTION__) + TEXT("(") + FString::FromInt(__LINE__) + TEXT(")"))
+#define ABLOG_S(Verbosity) UE_LOG(UE5Practice, Verbosity, TEXT("%s"), *ABLOG_CALLINFO)
+#define ABLOG(Verbosity, Format, ...) UE_LOG(UE5Practice, Verbosity, TEXT("%s %s"), *ABLOG_CALLINFO, *FString::Printf(Format, ##__VA_ARGS__))
